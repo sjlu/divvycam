5eab047b83f1f2e1e890604b869889b1df1de127
Steven Lu <tacticalazn@gmail.com>
2011-10-22 16:07:59 -0400

    Added in fields to joining and creating new buckets.

--- Resources/create.js
+++ Resources/create.js
@@ -4,7 +4,59 @@ Divvy.Create.init = function()
 {
 	this.win = Ti.UI.createWindow({
 		title: 'New Bucket',
+		barColor: '#333',
+		translucent: false
 	});
+	
+	this.row_bucketname = Ti.UI.createTableViewRow({
+		selectionStyle: Ti.UI.iPhone.TableViewCellSelectionStyle.NONE
+	});
+	
+	this.label_bucketname = Ti.UI.createLabel({
+		text: 'Name',
+		left: 10,
+		font: { fontSize: 16, fontWeight: 'bold' }
+	});
+	
+	this.textarea_bucketname = Ti.UI.createTextField({
+		hintText: 'My New Bucket',
+		left: 100,
+		width: 190, height: 24,
+		editable: true,
+		clearButtonMode: Titanium.UI.INPUT_BUTTONMODE_ONFOCUS,
+	});
+	
+	this.row_bucketname.add(this.textarea_bucketname);
+	this.row_bucketname.add(this.label_bucketname);
+	
+	this.row_bucketpw = Ti.UI.createTableViewRow({
+		selectionStyle: Ti.UI.iPhone.TableViewCellSelectionStyle.NONE
+	});
+	
+	this.label_bucketpw = Ti.UI.createLabel({
+		text: 'Password',
+		left: 10,
+		font: { fontSize: 16, fontWeight: 'bold' }
+	});
+	
+	this.textarea_bucketpw = Ti.UI.createTextField({
+		hintText: 'Optional',
+		left: 100,
+		width: 190, height: 24,
+		editable: true,
+		clearButtonMode: Titanium.UI.INPUT_BUTTONMODE_ONFOCUS,
+	});
+	
+	this.row_bucketpw.add(this.textarea_bucketpw);
+	this.row_bucketpw.add(this.label_bucketpw);
+	
+	this.tableview = Ti.UI.createTableView({
+		style: Ti.UI.iPhone.TableViewStyle.GROUPED,
+	});
+	
+	this.tableview.appendRow(this.row_bucketname);
+	this.tableview.appendRow(this.row_bucketpw);
+	this.win.add(this.tableview);
 };
 
 Divvy.Create.open = function()
--- Resources/join.js
+++ Resources/join.js
@@ -7,9 +7,60 @@ Divvy.Join.init = function()
 		barColor: '#333',
 		translucent: false
 	});
+	
+	this.row_bucketid = Ti.UI.createTableViewRow({
+		selectionStyle: Ti.UI.iPhone.TableViewCellSelectionStyle.NONE
+	});
+	
+	this.label_bucketid = Ti.UI.createLabel({
+		text: 'Bucket',
+		left: 10,
+		font: { fontSize: 16, fontWeight: 'bold' }
+	});
+	
+	this.textarea_bucketid = Ti.UI.createTextField({
+		hintText: '5-digit ID',
+		left: 100,
+		width: 190, height: 24,
+		editable: true,
+		keyboardType: Ti.UI.KEYBOARD_NUMBER_PAD,
+		clearButtonMode: Titanium.UI.INPUT_BUTTONMODE_ONFOCUS,
+	});
+	
+	this.row_bucketid.add(this.textarea_bucketid);
+	this.row_bucketid.add(this.label_bucketid);
+	
+	this.row_bucketpw = Ti.UI.createTableViewRow({
+		selectionStyle: Ti.UI.iPhone.TableViewCellSelectionStyle.NONE
+	});
+	
+	this.label_bucketpw = Ti.UI.createLabel({
+		text: 'Password',
+		left: 10,
+		font: { fontSize: 16, fontWeight: 'bold' }
+	});
+	
+	this.textarea_bucketpw = Ti.UI.createTextField({
+		hintText: 'Optional',
+		left: 100,
+		width: 190, height: 24,
+		editable: true,
+		clearButtonMode: Titanium.UI.INPUT_BUTTONMODE_ONFOCUS,
+	});
+	
+	this.row_bucketpw.add(this.textarea_bucketpw);
+	this.row_bucketpw.add(this.label_bucketpw);
+	
+	this.tableview = Ti.UI.createTableView({
+		style: Ti.UI.iPhone.TableViewStyle.GROUPED,
+	});
+	
+	this.tableview.appendRow(this.row_bucketid);
+	this.tableview.appendRow(this.row_bucketpw);
+	this.win.add(this.tableview);
 };
 
 Divvy.Join.open = function()
 {
-	
+	Divvy.open(this.win);
 };
