192ef220197721ce6d5d8d4e407548001c3a553f
Steven Lu <tacticalazn@gmail.com>
2011-10-28 01:58:34 -0400

    More oopsies.

--- web/api/image.php
+++ web/api/image.php
@@ -3,7 +3,7 @@ include_once '../include/config.php';
 
 function get_image($image_id, $duid)
 {
-   $image = db_query('SELECT bucket_id,filename FROM photos WHERE id="%s" LIMIT 1', $image_id);
+   $image = db_query('SELECT bucket_id, filename FROM photos WHERE id="%s" LIMIT 1', $image_id);
 
    if (count($image) < 1 || count($image) > 1)
       return array('status' => 'error', 'error' => 'no_such_image');
@@ -14,7 +14,7 @@ function get_image($image_id, $duid)
 	db_query('UPDATE buckets_devices SET last_activity=CURRENT_TIMESTAMP WHERE bucket_id="%s" AND duid="%s"', $image[0]['bucket_id'], $duid);
 
    $s3 = new AmazonS3();
-   return $s3->get_object_url('divvycam', $image[0]['filename'] . '.jpg', '60 seconds');
+	return array('status' => 'success', 'url' => $s3->get_object_url('divvycam', $image[0]['filename'] . '.jpg', '60 seconds'));
 }
 
 echo json_encode(get_image($_GET['image_id']));
