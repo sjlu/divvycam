98dc2dfcd13d41c35ead3a7bf73e0b1abe595654
Steven Lu <tacticalazn@gmail.com>
2011-11-05 11:10:18 -0400

    Added device UUID to the database for photo uploads.

--- web/api/upload.php
+++ web/api/upload.php
@@ -43,9 +43,9 @@ function upload_to_s3($file, $filename)
 
 function add_image_to_db($bucket_id, $filename, $duid)
 {
-   db_query('INSERT INTO photos (bucket_id, filename) VALUES ("%s","%s")', $bucket_id, $filename);
+   db_query('INSERT INTO photos (bucket_id, filename, duid) VALUES ("%s","%s","%s")', $bucket_id, $filename, $duid);
    db_query('UPDATE buckets SET last_updated=CURRENT_TIMESTAMP WHERE id="%s"', $bucket_id);
-	db_query('UPDATE buckets_devices SET last_activity=CURRENT_TIMESTAMP WHERE bucket_id="%s" AND duid="%s"', $bucket_id, $duid);
+   db_query('UPDATE buckets_devices SET last_activity=CURRENT_TIMESTAMP WHERE bucket_id="%s" AND duid="%s"', $bucket_id, $duid);
    return true;
 }
 
