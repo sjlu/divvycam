a96f8445cc93c67eba152e74c02bd8e82f20b57f
Steven Lu <tacticalazn@gmail.com>
2011-11-06 16:22:59 -0500

    Fixing delete bucket

--- web/api/delete_bucket.php
+++ web/api/delete_bucket.php
@@ -3,10 +3,12 @@ include_once '../include/config.php';
 
 function delete_bucket_reference($duid, $bucket_id)
 {
-	$query = db_query('SELECT COUNT(*) FROM buckets_devices WHERE duid="%s" AND bucket_id="%s"; DELETE FROM buckets_devices WHERE duid="%s" AND bucket_id="%s" LIMIT 1', $duid, $bucket_id, $duid, $bucket_id);
+	$query = db_query('SELECT COUNT(*) FROM buckets_devices WHERE duid="%s" AND bucket_id="%s"', $duid, $bucket_id);
 	
 	if ($query[0]['COUNT(*)'] > 1 || $query[0]['COUNT(*)'] < 1)
 		return '{"status":"error", "error":"device_not_registered_to_bucket"}';
+	
+	$query = db_query('DELETE FROM buckets_devices WHERE duid="%s" AND bucket_id="%s" LIMIT 1', $duid, $bucket_id);
 		
 	return '{"status":"success"}';
 }
