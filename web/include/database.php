237b5fb5fd82129b78dec0e4b8122b4461b73156
Steven Lu <tacticalazn@gmail.com>
2011-11-12 08:12:32 +0000

    Revert "using mysqli"
    This reverts commit cb4e5db1495dfcbf83a8ed41d25e9103ebff7a4a.

--- web/include/database.php
+++ web/include/database.php
@@ -1,12 +1,43 @@
 <?php
    //Use this only for development mode. Set this equal to 0 when moving to production mode.
-   $mysqli;
+   define('DEBUG', 1);
 
+   //show all php errors if debug is 1
+   if(DEBUG)
+   {
+      error_reporting(E_ALL);
+      ini_set('display_errors', '1');
+   }
+
+
+   /** This is a nice version of print_r you can use while debugging webpages. It'll print the array prettier. */
+   function debug_r($arr)
+   {
+      if(DEBUG)
+      {
+         echo '<pre>';
+         print_r($arr);
+         echo '</pre>';
+      }
+   }
+
+   /** This function prints out the html encoded version of text. If you are printing out a variable and you know it doesn't contain html, use encho() instead of echo(). Helps a lot in avoiding XSS bugs. */
+   function encho($text)
+   {
+      echo htmlspecialchars($text);
+   }
+   
    /** This function connects us to the database defined in the macros above. */
    function db_connect()
    {
-      global $mysqli;
-      $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME) or die("MySQL connection failed.");
+      $success = mysql_connect(DB_HOST, DB_USER, DB_PASS);     
+      if(!$success)
+         if(DEBUG)
+            exit('MySQL connection failed on line '. __LINE__. ' in file '. __FILE__. "\n");
+         else
+            echo 'The database connection failed';
+
+      mysql_select_db(DB_NAME);
    }
 
    /** This function is equivalent to calling mysql_query(sprintf(query, ...)). It will mysql_real_escape all args after the first arg in sprintf.
@@ -15,8 +46,6 @@
     */
    function db_query($query)
    {
-      global $mysqli;
-
       $num_args = func_num_args();
 
       $args = array();
@@ -29,17 +58,13 @@
 
       $query = call_user_func_array('sprintf', $args);
 
-      if (!$result = $mysqli->query($query))
-         log_error($mysqli->error, $query);
-      
-      $returnData = array();
+      $mReturnData = array();
+      $mResult = mysql_query($query) or die(log_error(mysql_error(), $query)); 
 
-      while($row = $result->fetch_array(MYSQLI_ASSOC))
-         $returnData[] = $row;
+      while($row = mysql_fetch_array($mResult, MYSQL_ASSOC))
+         $mReturnData[] = $row;
       
-      $result->close();
-
-      return $returnData;
+      return $mReturnData;
    }
    
    function log_error($error, $query)
