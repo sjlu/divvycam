c10fa19d13d09cd243259432f711a0d9bff266dc
Steven Lu <tacticalazn@gmail.com>
2011-11-11 22:43:19 -0500

    modifying files a little bit

--- web/include/config.php
+++ web/include/config.php
@@ -1,6 +1,22 @@
 <?php
+// Error handling
+function error_handler($number, $message, $file, $line, $vars)
+{
+   $email = 'An error occured in ' . $file . ' on line ' . $line . '.\n';
+   $email .= $message;
+
+   error_log($email, 1, 'slu@burst-dev.com');
+
+   if (($number !== E_NOTICE) && ($number < 2048))
+      die("An error has occured, please try again later.");
+}
+
+set_error_handler('error_handler');
+
+// Need amazon classes
 include 'aws/sdk.class.php';
 
+// Setting up the DB
 if (isset($_SERVER['db_hostname']))
 {
    define('DB_HOST', getenv('db_hostname'));
@@ -17,29 +33,21 @@ define('DB_NAME', 'divvy');
 define('DB_USER', 'divvy');
 define('DB_PASS', 'sRYbwWjK2t6DGTTH');
 
+// Just a token we can use
 define('BLOWFISH_SECRET', '4rehAJasaPrafrU2a4e4wapraS89CRa6');
 
+// Connecting to the database
 include 'database.php';
 db_connect();
 
+// No need to show the user the error
 ini_set('display_errors', 'Off');
 //session_start();
 
-function error_handler($number, $message, $file, $line, $vars)
-{
-   $email = 'An error occured in ' . $file . ' on line ' . $line . '.\n';
-   $email .= $message;
-
-   error_log($email, 1, 'slu@burst-dev.com');
-
-   if (($number !== E_NOTICE) && ($number < 2048))
-      die("An error has occured, please try again later.");
-}
-
-set_error_handler('error_handler');
-
+// include global helpers
 include_once('helpers.php');
 
+// include any other sub helpers
 $info = pathinfo($_SERVER['PHP_SELF']);
 $filename = basename($_SERVER['PHP_SELF'], '.' . $info['extension']);
 
